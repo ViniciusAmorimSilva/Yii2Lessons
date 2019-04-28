d600a2d0698a6e19b8fb721d90094fb074051bbf
viniciusamorimsilva <amorimm.vinicius@gmail.com>
2019-04-28 20:08:01 -0300

    Yii2 Lesson - 15 DatePicker in the Filter Field Gridview

--- backend/modules/settings/views/companies/index.php
+++ backend/modules/settings/views/companies/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\CompaniesSearch */
@@ -30,6 +31,20 @@ $this->params['breadcrumbs'][] = $this->title;
             'company_name',
             'company_email:email',
             'company_address',
+            [
+                'attribute' => 'company_start_date',
+                'value' => 'company_start_date',
+                'format' => 'raw',
+                'filter' => DatePicker::widget([
+                    'model' => $searchModel,
+                    'attribute' => 'company_start_date',
+                    //'template' => '{addon}{input}',
+                    'clientOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-m-d'
+                    ]
+                ])
+            ],
             'logo',
             //'company_start_date',
             //'company_created_date',
